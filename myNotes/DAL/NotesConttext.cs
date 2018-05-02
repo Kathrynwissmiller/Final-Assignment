eaff4631dd4d64d209abd60e645b7a7bb1c6a052
KATHRYNWISS9056\kathryn <wissmillerk06@lsus.edu>
2018-05-02 17:52:10 -0500

    Final Assignment

---
+++ myNotes/DAL/NotesConttext.cs
@@ -0,0 +1,19 @@
+﻿using myNotes.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace myNotes.DAL
+{
+    public class NotesConttext : DbContext
+    {
+        public NotesConttext() : base("myNotesDatabase")
+        {
+        }
+
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Notes> Notes { get; set; }
+        public DbSet<Categories> Categories { get; set; }
+
+
+    }
+}
---
+++ myNotes/DAL/NotesInitializer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using myNotes.Models;
+
+namespace myNotes.DAL
+{
+    public class NotesInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<NotesConttext>
+    {
+
+        protected override void Seed(NotesConttext context)
+        {
+            var users = new List<Users>
+            {
+                new Users{ID = 1, Name = "John Doe", Email = "testemail@ttest.com",
+                CreatedOn = "string" }, 
+                new Users{ID = 2, Name = "Alaska", Email = "snake@slither.com",
+                CreatedOn = "string" },
+                new Users{ID = 3, Name = "Alyssa Edwards", Email = "beyondbelief@aol.com",
+                CreatedOn = "string" }
+
+            };
+
+            context.SaveChanges();
+
+            var notes = new List<Notes>
+            {
+                new Notes{ID = 1, Title = "Title", Note = "notte", CategoryID = 1,
+                CreatedOn = "Monday", IsDeleted = "Maybe"},
+                new Notes{ID = 2, Title = "This is why I'm a Snake", Note = "Because I won AS3", CategoryID = 2,
+                CreatedOn = "Monday", IsDeleted = "Maybe"},
+                 new Notes{ID = 3, Title = "Unique Title", Note = "Unique notte", CategoryID = 3,
+                CreatedOn = "Yesterday", IsDeleted = "Nope"}
+            };
+
+            context.SaveChanges();
+
+            var categories = new List<Categories>
+            {
+                new Categories{ID = 1, Name = "Home" },
+                new Categories{ID = 2, Name = "SnakesOnly" },
+                new Categories{ID = 3, Name = "Work" }
+            };
+
+            context.SaveChanges();
+        }
+    }
+}
